f5ff09a385f03183752d0c0cc974ef7bd0d6bba9
Valentina Vlad <vlad.valentina.diana@gmail.com>
2020-09-22 17:53:58 +0300

    added templates folder with layout.html

--- BlogApp/app.py
+++ BlogApp/app.py
@@ -1,13 +1,37 @@
-from flask import Flask, url_for, request
+from flask import Flask, url_for, request, render_template
 from markupsafe import escape
 
 app = Flask(__name__)
 
-@app.route('/')
+posts = [
+    {
+        'id': 1,
+        'title': 'Python',
+        'contents': 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor.',
+        'owner': 'D.B.Higgins',
+        'created_at': 'September 20, 2020',
+        'modified_at': 'September 21, 2020'
+    },
+    {
+        'id': 2,
+        'title': 'Php',
+        'contents': 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor.',
+        'owner': 'D.B.Higgins',
+        'created_at': 'September 19, 2020',
+        'modified_at': 'September 21, 2020'
+    }
+]
 
-def hello():
-    return '<h1>Hello world!!</h1>'
+@app.route('/')
+@app.route('/home')
+def home():
+    return render_template('home.html', posts=posts)
+
+@app.route('/add_post')
+def add_post():
+    return render_template('post.html')
+
 
 if __name__ == '__main__':
     # Run the app server on localhost:4449
-    app.run('localhost', 4449)
+    app.run(debug=True)
