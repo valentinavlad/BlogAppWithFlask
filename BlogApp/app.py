4b93d83b689bed3f4c475450a256eb334199f5a6
Valentina Vlad <vlad.valentina.diana@gmail.com>
2020-09-24 12:03:25 +0300

    Added navigation and modified the add post function

--- BlogApp/app.py
+++ BlogApp/app.py
@@ -1,56 +1,53 @@
-from flask import Flask, url_for, request, render_template
+from flask import Flask, url_for, request, render_template, redirect
 from markupsafe import escape
 import json
 import datetime
+from posts_data import posts
 import random
 
 app = Flask(__name__)
 
-
-# function to add to JSON 
-def write_json(data, filename='posts2.json'): 
-    with open(filename,'w') as f: 
-        json.dump(data, f, indent=4) 
-      
-
 @app.route('/')
 @app.route('/home')
 def home():
-    # Opening JSON file 
-    with open('posts2.json', 'r') as openfile: 
-        # Reading from json file 
-        posts = json.load(openfile) 
-    print(posts)
     return render_template('home.html', posts=posts)
 
 @app.route('/add_post', methods=['GET','POST'])
 def add_post():
     if request.method == 'POST':
-        req = request.form
         date_now = datetime.datetime.now()
-        with open('posts2.json') as json_file: 
-            data = json.load(json_file) 
-            temp = data 
-  
-            # python object to be appended 
-            y = {
-                "id": random.randint(1, 9),
-                "title": request.form.get("title"),
-                "contents": request.form.get("contents"),
-                "owner": request.form.get("owner"),
-                "created_at": date_now.strftime("%Y, %B, %A"),
-                "modified_at":  date_now.strftime("%Y, %B, %A")
-            }
-  
-            # appending data to emp_details  
-            temp.append(y) 
-      
-        write_json(data)  
+ 
+        post = {
+            "id": random.randint(1, 9),
+            "title": request.form.get("title"),
+            "contents": request.form.get("contents"),
+            "owner": request.form.get("owner"),
+            "created_at": date_now.strftime("%Y, %B, %A"),
+            "modified_at":  date_now.strftime("%Y, %B, %A")
+        }
+        posts.append(post)
+        return redirect(url_for('home'))
     return render_template('post.html')
 
-#with open('posts2.json', 'w') as outfile:
- #   json.dump(data, outfile)
+@app.route('/edit', methods=['GET','POST'])
+def edit():
+    if request.method == 'POST':
+        date_now = datetime.datetime.now()
+ 
+        post = {
+            "id": random.randint(1, 9),
+            "title": request.form.get("title"),
+            "contents": request.form.get("contents"),
+            "owner": request.form.get("owner"),
+            "created_at": date_now.strftime("%Y, %B, %A"),
+            "modified_at":  date_now.strftime("%Y, %B, %A")
+        }
+        posts.append(post)
+    return render_template('edit.html')
 
+    
+    
+    
 if __name__ == '__main__':
     # Run the app server on localhost:4449
     app.run(debug=True)
---
+++ BlogApp/posts_data.py
@@ -0,0 +1,35 @@
+posts = [
+    {
+        'id': 1,
+        'title': 'Python',
+        'contents': 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor.',
+        'owner': 'D.B.Higgins',
+        'created_at': 'September 20, 2020',
+        'modified_at': 'September 21, 2020'
+    },
+    {
+        'id': 2,
+        'title': 'Php',
+        'contents': 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor.',
+        'owner': 'D.B.Higgins',
+        'created_at': 'September 19, 2020',
+        'modified_at': 'September 21, 2020'
+    },
+    {
+        'id': 3,
+        'title': 'Java',
+        'contents': 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor.',
+        'owner': 'D.B.Higgins',
+        'created_at': 'April 8, 2020',
+        'modified_at': 'September 9, 2020'
+    },
+    {
+        'id': 4,
+        'title': 'C#',
+        'contents': 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor.',
+        'owner': 'K.J. Hook',
+        'created_at': 'May 18, 2020',
+        'modified_at': 'Iune 9, 2020'
+    }
+]
+
