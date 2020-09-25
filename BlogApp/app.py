00578a054e4d90f2d019e3219dad4b53592cc227
Valentina Vlad <vlad.valentina.diana@gmail.com>
2020-09-25 13:20:25 +0300

    Resolved errors

--- BlogApp/app.py
+++ BlogApp/app.py
@@ -10,9 +10,8 @@ from flask import Response
 app = Flask(__name__)
 
 @app.route('/')
-@app.route('/posts', methods=['GET','POST'])
+@app.route('/posts/', methods=['GET','POST'])
 def posts():
-  
     return render_template('posts.html', content=dummy_posts)
 
 print(dummy_posts)
@@ -29,7 +28,6 @@ def new():
         return redirect(url_for('posts'))
     return render_template('post.html')
   
-
 @app.route('/posts/<int:id>/edit', methods=['GET','POST'])
 def edit(id):
     for post in dummy_posts:
@@ -39,14 +37,15 @@ def edit(id):
         date_now = datetime.datetime.now()
         post = Post(title=request.form.get("title"), owner= request.form.get("owner"),
                     contents=request.form.get("contents"),
-                    created_at=date_now.strftime("%Y, %B, %A"),
-                    modified_at=date_now.strftime("%Y, %B, %A"))
+                    created_at=date_now.strftime("%B %d, %Y"),
+                    modified_at=date_now.strftime("%B %d, %Y"))
         dummy_posts.remove(found_post)
         dummy_posts.append(post)
         return redirect(url_for('posts'))
     return render_template('edit.html', post=found_post)
 
-@app.route('/posts/<int:id>/delete', methods=['GET','DELETE'])
+# doesn't work
+@app.route('/posts/<int:id>/delete', methods=['POST'])
 def delete(id):
     deleted = False
     for post in dummy_posts:
