992ceefad8d6619e2c7af01ff3665d3e3c1128be
Valentina Vlad <vlad.valentina.diana@gmail.com>
2020-09-29 20:15:10 +0300

    Implemented view_posts in db class, need to revisit function

--- BlogApp/repository/repo_db.py
+++ BlogApp/repository/repo_db.py
@@ -1,6 +1,30 @@
 from repository.interface_repo import InterfaceRepo
+import psycopg2
 
 class RepoDb(InterfaceRepo):
     """description of class"""
     def __init__(self):
         pass
+    def add(self):
+        sql = "INSERT INTO posts(title, owner,contents,created_at,modified_at) VALUES(%s,%s,%s,%s,%s)"
+        # ??? 
+    def view_posts(self):
+        conn = None
+        try:
+            params = config()
+            conn = psycopg2.connect(**params)
+            cur = conn.cursor()
+            cur.execute("SELECT * FROM posts ORDER BY created_at")
+            print("The number of parts: ", cur.rowcount)
+            row = cur.fetchone()
+
+            while row is not None:
+                print(row)
+                row = cur.fetchone()
+
+            cur.close()
+        except (Exception, psycopg2.DatabaseError) as error:
+            print(error)
+        finally:
+            if conn is not None:
+                conn.close()
