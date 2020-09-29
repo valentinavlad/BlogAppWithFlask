b59938e3a1951bff0c1b34c7b074a489f7208387
Valentina Vlad <vlad.valentina.diana@gmail.com>
2020-09-29 19:30:27 +0300

    Added test method

--- BlogApp/tests/test_flaskr.py
+++ BlogApp/tests/test_flaskr.py
@@ -1,3 +1,8 @@
 def test_index(app, client):
     res = client.get('/')
     assert res.status_code == 200
+    assert '<title>Blog posts</title>' in res.get_data(as_text=True)
+
+def test_view_post(app, client):
+    res = client.get('/posts/5')
+    assert res.status_code == 200
