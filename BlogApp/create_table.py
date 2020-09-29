0c809dfeafbbc77fc5367c586c5c22814c0eb895
Valentina Vlad <vlad.valentina.diana@gmail.com>
2020-09-29 13:27:35 +0300

    Changed route for cancel button on view_post page, added create_table file

---
+++ BlogApp/create_table.py
@@ -0,0 +1,38 @@
+import psycopg2
+from config import config
+
+
+def create_table():
+    """ create tables in the PostgreSQL database"""
+    command = """
+        CREATE TABLE posts (
+            post_id SERIAL PRIMARY KEY,
+            title VARCHAR(255) NOT NULL,
+            owner VARCHAR(255) NOT NULL,
+            contents VARCHAR(255) NOT NULL,
+            created_at DATE NOT NULL DEFAULT CURRENT_DATE,
+            modified_at DATE NULL
+            )
+        """
+    conn = None
+    try:
+        # read the connection parameters
+        params = config()
+        # connect to the PostgreSQL server
+        conn = psycopg2.connect(**params)
+        cur = conn.cursor()
+        # create table
+        cur.execute(command)
+        # close communication with the PostgreSQL database server
+        cur.close()
+        # commit the changes
+        conn.commit()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        if conn is not None:
+            conn.close()
+
+
+if __name__ == '__main__':
+    create_table()
