3976af2195b2e8c062c130d8d92264c3e3279965
Valentina Vlad <vlad.valentina.diana@gmail.com>
2020-10-03 18:40:22 +0300

    Test for add a post

--- BlogApp/tests/test_flaskr.py
+++ BlogApp/tests/test_flaskr.py
@@ -1,7 +1,9 @@
+
 def test_index(client):
     res = client.get('/')
     assert res.status_code == 200
-    assert '<title>Blog posts</title>' in res.get_data(as_text=True)
+    assert '<h2>Check our latest posts in web technologies!</h2>' in res.get_data(as_text=True)
+    assert '<h1>Angular</h1>' in res.get_data(as_text=True)
 
 def test_view_post(client):
     res = client.get('/posts/5')
@@ -11,3 +13,9 @@ def test_view_post(client):
 def test_all_posts(client):
     res = client.get('/posts')
     assert res.status_code == 308
+
+def test_post_create(client):
+    content_header = {'Content-Type': 'text/html'}
+    data = {'title': 'test-file', 'contents':'hello'}
+    r = client.post('/posts/new', headers=content_header, json=data)
+    assert r.status_code == 302
