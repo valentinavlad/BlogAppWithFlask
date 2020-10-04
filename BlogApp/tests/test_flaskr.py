0e8192d6aac0b4a8805b03176cda4899aa00205c
Valentina Vlad <vlad.valentina.diana@gmail.com>
2020-10-04 16:33:13 +0300

    Added test for delete post

--- BlogApp/tests/test_flaskr.py
+++ BlogApp/tests/test_flaskr.py
@@ -1,28 +1,28 @@
 from flask import url_for
 
 def test_index(client):
-    res = client.get('/', follow_redirects=True)
-    assert res.status_code == 200
-    assert '<h1>Angular</h1>' in res.get_data(as_text=True)
-    assert '<h1>Php</h1>' in res.get_data(as_text=True)
-    assert b'Check our latest posts in web technologies!' in res.data
-    assert b'Add a post' in res.data
+    response = client.get('/', follow_redirects=True)
+    assert response.status_code == 200
+    assert '<h1>Angular</h1>' in response.get_data(as_text=True)
+    assert '<h1>Php</h1>' in response.get_data(as_text=True)
+    assert b'Check our latest posts in web technologies!' in response.data
+    assert b'Add a post' in response.data
 
 def test_view_post(client):
-    res = client.get('/posts/5')
-    assert '<h3>Angular</h3>' in res.get_data(as_text=True)
-    assert b'V.W. Craig' in res.data
-    assert res.status_code == 200
+    response = client.get('/posts/5')
+    assert '<h3>Angular</h3>' in response.get_data(as_text=True)
+    assert b'V.W. Craig' in response.data
+    assert response.status_code == 200
 
 def test_all_posts(client):
-    res = client.get('/posts')
-    assert res.status_code == 308
+    response = client.get('/posts')
+    assert response.status_code == 308
 
 def test_post_create(client):
-    res = client.get('/posts/new')
-    assert res.status_code == 200
-    assert b'Owner' in res.data
-    assert b'Content' in res.data
+    response = client.get('/posts/new')
+    assert response.status_code == 200
+    assert b'Owner' in response.data
+    assert b'Content' in response.data
 
     content_header = {'Content-Type': 'text/html'}
     data = {'title': 'KOKO', 'contents':'hello'}
@@ -33,9 +33,9 @@ def test_post_create(client):
         assert 'KOKO' in r.get_data(as_text=True)
   
 def test_update_post(client):
-    r1 = client.get('/posts/2')
-    assert r1.status_code == 200
-    assert b'Edit your post' in r1.data
+    response = client.get('/posts/2')
+    assert response.status_code == 200
+    assert b'Edit your post' in response.data
    
     data={'title': 'updated PHP', 'owner': 'update'}
     with client as c:
@@ -45,10 +45,14 @@ def test_update_post(client):
         assert 'updated PHP' in r.get_data(as_text=True)
    
 def test_delete_post(client):
-    r1 = client.get('/posts/4')
-    assert r1.status_code == 200
-    assert b'Delete post' in r1.data
+    #at id 4 is Javascript
+    res = client.get('/posts/4')
+    assert res.status_code == 200
+    assert b'Delete your post' in res.data
     with client as c:
-        r = client.post('/posts/4', data=data, follow_redirects=True)
-        assert r.status_code == 302
+        response = client.post('/posts/4/delete',follow_redirects=True)
+        assert response.status_code == 200
+        assert b'Javascript' not in response.data
+        assert '<h1>Angular</h1>' in response.get_data(as_text=True)
+        assert '<h1>Php</h1>' in response.get_data(as_text=True)
         
