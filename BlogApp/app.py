a75d3a9e067b2537d6627e6c2cf44a0a09fc911a
Valentina Vlad <vlad.valentina.diana@gmail.com>
2020-10-15 12:26:34 +0300

    Added login.html page

--- BlogApp/app.py
+++ BlogApp/app.py
@@ -4,10 +4,13 @@ from flask_injector import FlaskInjector
 from services.dependencies import configure_production
 from views.posts_view import index_blueprint
 from views.setup_view import setup_blueprint
+from views.users_view import users_blueprint
 
 app = Flask(__name__)
+
 app.register_blueprint(index_blueprint, url_prefix="/posts")
 app.register_blueprint(setup_blueprint, url_prefix="/setup")
+app.register_blueprint(users_blueprint, url_prefix="/user")
 
 @app.route('/')
 def index():
@@ -16,5 +19,5 @@ def index():
 FlaskInjector(app=app, modules=[configure_production])
 
 if __name__ == '__main__':
-    #app.run(debug=True)
-    app.run('localhost', 4449)
+    app.run(debug=True)
+    #app.run('localhost', 4449)
--- BlogApp/views/users_view.py
+++ BlogApp/views/users_view.py
@@ -1,6 +1,8 @@
 from flask import Blueprint, render_template, url_for, request, redirect
 
-users_blueprint = Blueprint('users_blueprint', __name__, template_folder='templates',
+users_blueprint = Blueprint('users', __name__, template_folder='templates',
                             static_folder='static')
 
 @users_blueprint.route('/login', methods=('GET', 'POST'))
+def login():
+    return render_template('login.html')
