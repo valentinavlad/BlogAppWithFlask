e704fb8ea6c1011283c68b31e6b2fd4ec223d0b5
Valentina Vlad <vlad.valentina.diana@gmail.com>
2020-10-16 17:26:58 +0300

    Added html pages for crud users and functionality

--- BlogApp/views/users_view.py
+++ BlogApp/views/users_view.py
@@ -1,3 +1,4 @@
+import datetime
 from injector import inject
 from flask import Blueprint, render_template, url_for, \
     request, redirect, flash, session, g
@@ -9,3 +10,35 @@ users_blueprint = Blueprint('users', __name__, template_folder='templates', stat
 @users_blueprint.route('/', methods=['GET', 'POST'])
 def users(repo: UsersRepo):
      return render_template('list_users.html', content=repo.view_all())
+
+@inject
+@users_blueprint.route('/<int:pid>', methods=['GET'])
+def view_user(repo: UsersRepo, pid):
+    user = repo.find_by_id(pid)
+    return render_template('view_user.html', user=user)
+
+@inject
+@users_blueprint.route('/<int:pid>/edit', methods=['GET', 'POST'])
+def edit(repo: UsersRepo, pid):
+    found_user = repo.find_by_id(pid)
+    if request.method == 'POST':
+        if found_user is not None:
+            date_now = datetime.datetime.now()
+            user = found_user
+            user.name = request.form.get("name")
+            user.email = request.form.get("email")
+            user.password = request.form.get("password")
+            user.created_at = found_user.created_at
+            user.modified_at = date_now.strftime("%B %d, %Y")
+            repo.edit(user)
+        return redirect(url_for('users.view_user', pid=user.user_id))
+    return render_template('edit_user.html', user=found_user)
+
+@inject
+@users_blueprint.route('/<int:pid>/delete', methods=['GET', 'POST'])
+def delete(repo: UsersRepo, pid):
+    user_delete = repo.find_by_id(pid)
+    if user_delete is not None:
+        repo.delete(pid)
+        return redirect(url_for('users.users'))
+    return render_template('view_user.html')
